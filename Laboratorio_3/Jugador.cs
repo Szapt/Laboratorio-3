5b0a7c61e7b92eb793107e5236caef67ac36ed65
MiguelVargass82 <147115772+MiguelVargass82@users.noreply.github.com>
2023-10-15 16:35:38 -0500

    Borrar todo
    Vamos a iniciar de cero

--- Laboratorio_3/Jugador.cs
+++ Laboratorio_3/Jugador.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Labo3
 {
-    internal class Jugador
+    public internal class Jugador
     {
         String nombre;
         String correo;
---
+++ Laboratorio_3/Labo3/AmigoSecreto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo3
+{
+    internal class AmigoSecreto
+    {
+        int numJugadores;
+        string fechaInicial;
+        string Descubrimiento;
+        string numEndulzadas;
+        string frecuencia;
+        string valorEndulzada;
+        string valorRegalo;
+
+        
+
+
+    }
+}
